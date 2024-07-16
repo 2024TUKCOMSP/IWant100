c42bfbb6a6f79275b068d297f131edec7434ae94
김민지 <alswlk0306@gmail.com>
2024-07-16 16:19:56 +0900

    setting: react + vite, tailwind

---
+++ IWant100_FE/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-white">
+        Hello, Tailwind CSS with Vite and React!
+      </h1>
+    </div>
+  )
+}
+
+export default App
