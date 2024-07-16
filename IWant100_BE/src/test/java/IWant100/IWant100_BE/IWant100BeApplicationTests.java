a383aad51bbaf285eb926510325e1726e7ec18ea
yoosuhyeon <dbtngus20@naver.com>
2024-07-16 16:35:00 +0900

    feat : make project

---
+++ IWant100_BE/src/test/java/IWant100/IWant100_BE/IWant100BeApplicationTests.java
@@ -0,0 +1,13 @@
+package IWant100.IWant100_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IWant100BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
