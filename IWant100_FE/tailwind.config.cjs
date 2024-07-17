c4248ee562cedfa0c0e0124d562ed48fa301acb9
김민지 <alswlk0306@gmail.com>
2024-07-17 17:21:14 +0900

    style: default color

--- IWant100_FE/tailwind.config.cjs
+++ IWant100_FE/tailwind.config.cjs
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          900: '#E8ECFA',
+          800: '#F00E00',
+          700: '#008FF6',
+          600: '#D4E1FA',
+          500: '#CCEAFF',
+        }
+      },
+      backgroundImage: {
+      },
+    },
   },
   plugins: [],
 }
