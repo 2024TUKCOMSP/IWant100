10bc97fda2e86e44fd53e69ae5d5868f93a16672
yoosuhyeon <dbtngus20@naver.com>
2024-07-17 22:53:39 +0900

    feat : deleteVote

---
+++ IWant100_BE/src/main/java/IWant100/IWant100_BE/vote/bean/small/GetVoteItemsDAOBean.java
@@ -0,0 +1,24 @@
+package IWant100.IWant100_BE.vote.bean.small;
+
+import IWant100.IWant100_BE.vote.domain.VoteItem;
+import IWant100.IWant100_BE.vote.repository.VoteItemRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class GetVoteItemsDAOBean {
+    VoteItemRepositoryJPA voteItemRepositoryJPA;
+
+    @Autowired
+    public GetVoteItemsDAOBean(VoteItemRepositoryJPA voteItemRepositoryJPA) {
+        this.voteItemRepositoryJPA = voteItemRepositoryJPA;
+    }
+
+    // voteId를 통해 원하는 객체들 찾기
+    public List<VoteItem> exec(UUID voteId) {
+        return voteItemRepositoryJPA.findAllByVoteId(voteId);
+    }
+}
--- IWant100_BE/src/main/java/IWant100/IWant100_BE/vote/repository/VoteRepositoryJPA.java
+++ IWant100_BE/src/main/java/IWant100/IWant100_BE/vote/repository/VoteRepositoryJPA.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface VoteRepositoryJPA extends JpaRepository<Vote, UUID> {
+    Vote findByVoteIdAndUserId(UUID voteId, UUID userId);
 }
