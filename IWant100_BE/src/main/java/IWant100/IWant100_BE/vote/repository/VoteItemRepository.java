b0fb9f52c04f8767e9912de7c078f8340d578b43
yoosuhyeon <dbtngus20@naver.com>
2024-07-17 16:54:54 +0900

    Revert "feat : create DAO"
    This reverts commit 429dcabb116424994a7cf3454d15c21943e03f15.

--- IWant100_BE/src/main/java/IWant100/IWant100_BE/vote/repository/VoteItemRepository.java
+++
@@ -1,9 +0,0 @@
-package IWant100.IWant100_BE.vote.repository;
-
-import IWant100.IWant100_BE.vote.domain.VoteItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface VoteItemRepository extends JpaRepository<VoteItem, UUID> {
-}
--- IWant100_BE/src/main/java/IWant100/IWant100_BE/vote/service/VoteService.java
+++
@@ -1,7 +0,0 @@
-package IWant100.IWant100_BE.vote.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class VoteService {
-}
