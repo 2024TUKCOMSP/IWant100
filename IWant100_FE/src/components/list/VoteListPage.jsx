6f619361488e2545a22274d0b981d85843900b1e
seung yeon <137742302+moanuna@users.noreply.github.com>
2024-07-19 07:21:40 +0900

    feat: vote list page

--- IWant100_FE/src/components/list/VoteListPage.jsx
+++ IWant100_FE/src/components/list/VoteListPage.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import Header from "./ListHeader.jsx";
+import SearchInputBox from "./SearchInputBox.jsx";
+import VoteList from "./VoteList.jsx";
 
 function VoteListPage() {
   return (
     <div>
-      <div className=""></div>
+      <div className="flex flex-col w-full min-h-screen h-full bg-primary-900">
+        <div className="bg-blue-500 h-[285px] shadow-default rounded-b-[50%_20%] p-5">
+          <div>
+            <Header title="투표 목록" />
+          </div>
+          <SearchInputBox />
+        </div>
+        <VoteList /> 
+      </div>
     </div>
-  )
+  );
 }
 
 export default VoteListPage;
