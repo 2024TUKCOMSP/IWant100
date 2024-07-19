017d2d5a0bf2448daa6121df1db4c791f6fb6cdd
seung yeon <137742302+moanuna@users.noreply.github.com>
2024-07-20 02:42:22 +0900

    feat: result page

---
+++ IWant100_FE/src/components/result/VoteTitle.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function VoteTitle() {
+  return (
+    <div>
+      <div className="w-full h-[130px] bg-white rounded-xl mt-[70px] shadow-default p-[12px_24px_24px_24px] flex flex-col justify-between" >
+        <div className="font-esamanru text-xl p-2 w-full">앱 디자인이 예쁜가요?</div>
+        <div class="relative max-w-sm">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none"></div>
+        </div>
+        <span className="self-end break-words font-['Poppins'] font-bold text-[18px] underline tracking-[0px] leading-[1.111] text-[#414D55]">투표 마감 기한 : 24.07.18</span>
+      </div>
+    </div>
+  )
+}
+
+export default VoteTitle;
