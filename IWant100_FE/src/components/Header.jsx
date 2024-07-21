f1e99d2a96e107c62746fcbf18183e86490672c7
김민지 <alswlk0306@gmail.com>
2024-07-21 21:34:29 +0900

    feat: create vote page back arrow click event

--- IWant100_FE/src/components/Header.jsx
+++ IWant100_FE/src/components/Header.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 function Header(props) {
+  const navigate = useNavigate();
+
+  const handleBackArrow = () => {
+    navigate('/');
+  };
+
   return (
     <div>
       <div className="flex flex-row justify-between items-center">
-        <div className="w-8 h-8 bg-back-arrow-icon bg-no-repeat"></div>
+        <div
+          className="w-8 h-8 bg-back-arrow-icon bg-no-repeat cursor-pointer"
+          onClick={handleBackArrow}
+        ></div>
         <div className="font-esamanru text-white text-2xl">{props.title}</div>
         <div className="w-6 h-6 bg-submit-icon bg-no-repeat"></div>
       </div>
