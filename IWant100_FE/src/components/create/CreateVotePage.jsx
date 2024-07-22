32d20aa223e9e4ee923513a3712a491cf1746ea6
김민지 <alswlk0306@gmail.com>
2024-07-23 05:57:54 +0900

    style: change component name

--- IWant100_FE/src/components/create/CreateVotePage.jsx
+++ IWant100_FE/src/components/create/CreateVotePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../Header.jsx";
 import InputBox from "./InputBox.jsx";
-import VoteItem from "./VoteItem.jsx";
+import VoteItem from "./CreateVoteItem.jsx";
 
 function CreateVotePage() {
   return (
