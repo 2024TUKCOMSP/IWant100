6c4a39c0784825dfbb6ec458df27599d4b3ff40b
김민지 <alswlk0306@gmail.com>
2024-07-25 15:38:17 +0900

    fix: font style error

--- IWant100_FE/src/components/vote/VoteItem.jsx
+++ IWant100_FE/src/components/vote/VoteItem.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function VoteItem(props) {
+function VoteItem() {
   return (
     <div>
-      {[...Array(6)].map((_) => (
-        <div className="w-full mb-5 last:mb-0 font-esamanru p-4 bg-white rounded-lg flex justify-between items-center shadow-default cursor-pointer active:bg-primary-700 active:text-white">
+      {[...Array(6)].map((_, index) => (
+        <div key={index} className="w-full mb-5 last:mb-0 font-esamanru p-4 bg-white rounded-lg flex justify-between items-center shadow-default cursor-pointer active:bg-primary-700 active:text-white">
           <div>1. 매우 그렇다.</div>
         </div>
       ))}
