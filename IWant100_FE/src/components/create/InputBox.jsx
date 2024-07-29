beaf939105bfcdcfd9d18964e5f3c66351df8108
김민지 <alswlk0306@gmail.com>
2024-07-30 03:23:38 +0900

    style: create vote page data handling

--- IWant100_FE/src/components/create/InputBox.jsx
+++ IWant100_FE/src/components/create/InputBox.jsx
@@ -3,14 +3,25 @@ import CustomCalendar from "./CustomCalendar";
 
 function InputBox() {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [voteTitle, setVoteTitle] = useState('');
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   }
+
+  const saveTitle = (e) => {
+    setVoteTitle(e.target.value);
+  }
+
   return (
     <div>
       <div className="w-full h-[180px] bg-white rounded-xl mt-[70px] shadow-default p-6 flex flex-col justify-between">
-        <input placeholder="생성할 투표 제목을 입력하세요" className="font-esamanru text-xl p-2 w-full" />
+        <input 
+          placeholder="생성할 투표 제목을 입력하세요"
+          className="font-esamanru text-xl p-2 w-full"
+          value={voteTitle}
+          onChange={saveTitle}
+        />
         <CustomCalendar onChange={handleDateChange} value={selectedDate} />
       </div>
     </div>
