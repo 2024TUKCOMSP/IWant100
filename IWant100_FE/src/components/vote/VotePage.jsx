d6f2321a1069954fba90ddb44df311480fa41fb2
김민지 <alswlk0306@gmail.com>
2024-07-30 03:22:44 +0900

    style: modify when duplication is false, svg color

--- IWant100_FE/src/components/vote/VotePage.jsx
+++ IWant100_FE/src/components/vote/VotePage.jsx
@@ -57,7 +57,7 @@ function VotePage() {
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle 
                   cx="12" cy="12" r="12"
-                  className={`${vote.isDuplication ? 'fill-primary-700' : 'fill-secondary-900'}`}
+                  className={`${vote.isDuplication ? 'fill-primary-700' : 'fill-[#CCCCCC]'}`}
                 />
                 <path d="M7.19995 12L10.8 15.6L18 8.39999" stroke="white" strokeWidth="2" strokeLinecap="round"/>
               </svg>  
